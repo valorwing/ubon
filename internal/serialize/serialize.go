f2cc3f51adbf3151bed8499d338af4e24c7538d8
Oleksii Petrov <a.valorwing@gmail.com>
2024-08-14 20:22:09 +0300

    ubon types detection in progress. uint4 compression in progress

--- internal/serialize/serialize.go
+++ internal/serialize/serialize.go
@@ -1,5 +1,18 @@
-package searialize
+package serialize
 
 func Serialize(input any) ([]byte, error) {
+
+	return nil, nil
+}
+
+func serializePrimitive() {
+
+}
+
+func serializeArray() {
+
+}
+
+func serializeObject() {
 
 }
